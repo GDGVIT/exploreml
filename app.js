06ac2dab46c9ff3910f59b5b8fd7f8a7ca4ba9c6
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-01-06 15:09:40 +0530

    index + FAQ

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res, next) => {
 	res.sendFile(`${__dirname}/site/index.html`);
 })
 
+app.get("/faq", (req, res, next) => {
+	res.sendFile(`${__dirname}/site/faq.html`);
+})
+
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Listening....")
 })
